bae76a5bd1c254581a7f61a5740cf99d02ed7f49
Sundholmare <filip.sundholm@utb.ecutbildning.se>
2021-04-27 14:55:01 +0200

    First draft

--- zocom-home-master/index.js
+++ zocom-home-master/index.js
@@ -1,7 +1,36 @@
 const { app } = require('./core'); 
+const { db, update } = require('./db');
 
 app.listen(3000, () => {
     console.log('API for smart home 1.1 up n running.')
 })
 
 /* CODE YOUR API HERE */
+
+app.get('/lights/:id', (req, res) => {
+    const state = req.query.state === 'on' ? true : false;
+
+    db.get('devices')
+    .find({type: 'Light', id: req.params.id})
+    .assign({on: state})
+    .value();
+
+    update();
+
+    res.send(`${req.params.id} är ${req.query.state}`)
+})
+
+
+app.get('/AC', (req, res) => {
+    const state = req.query.state === 'on' ? true : false;
+    const temp = req.query.temp;
+
+    db.get('devices')
+    .find({type: 'AC'})
+    .assign({on: state, temperature: temp})
+    .value();
+
+    update();
+
+    res.send(`AC:n är ${req.query.state}`)
+})
